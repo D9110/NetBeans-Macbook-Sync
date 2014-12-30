5865d97a0c45f7d227c0fe163ecf8f7818fc506b
Devanshu <devanshu911@gmail.com>
2014-12-30 23:18:00 +0530

    Update TempTest.java

--- TempTest.java
+++ TempTest.java
@@ -201,58 +201,9 @@ public class JavaFormatSourcesCommandTest
          }
 
       }
-      
-//      FileResource<?> fr;
-//       fr.createTempResource();
-//       
-//       
-//           
-       
-//     
-//     boolean dirFlag = false;
-//
-//  // create File object
-//  File stockDir = new File("d://Stock/ stockDir ");
-//
-//  try {
-//     dirFlag = stockDir.mkdir();
-//  } catch (SecurityException Se) {
-//  System.out.println("Error while creating directory in Java:" + Se);
-//  }
-//   
-
 
       
    }
    
-   
-   public void copyDirectory(File sourceLocation , File targetLocation)
-   throws IOException {
-       
-       if (sourceLocation.isDirectory()) {
-           if (!targetLocation.exists()) {
-               targetLocation.mkdir();
-           }
-           
-           String[] children = sourceLocation.list();
-           for (int i=0; i<children.length; i++) {
-               copyDirectory(new File(sourceLocation, children[i]),
-                       new File(targetLocation, children[i]));
-           }
-       } else {
-           
-           InputStream in = new FileInputStream(sourceLocation);
-           OutputStream out = new FileOutputStream(targetLocation);
-           
-           // Copy the bits from instream to outstream
-           byte[] buf = new byte[1024];
-           int len;
-           while ((len = in.read(buf)) > 0) {
-               out.write(buf, 0, len);
-           }
-           in.close();
-           out.close();
-       }
-   }
 
 }
